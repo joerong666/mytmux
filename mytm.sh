9d64ef6651dd5f6811a3f79adc6c6dfa567f9c4b
joerong <joerong@localhost.localdomain>
2014-02-12 13:55:03 +0800

    first commit

---
+++ mytm.sh
@@ -0,0 +1,61 @@
+#edit
+if [ "$1" = "-E" ]; then
+	tmux split-window -h
+	tmux resize-pane -R 35
+	tmux split-window -v
+	tmux select-pane -t 0
+	tmux split-window -v
+	tmux resize-pane -D 15
+	tmux select-pane -t 0
+fi
+
+#Quarter
+if [ "$1" = "-Q" ]; then
+	tmux split-window -v
+	tmux split-window -h
+	tmux select-pane -t 0
+	tmux split-window -h
+	tmux select-pane -t 0
+
+	tmux next-layout
+	tmux next-layout
+	tmux next-layout
+fi
+
+#monitor
+if [ "$1" = "-M" ]; then
+	tmux split-window -v
+	tmux split-window -h
+	tmux select-pane -t 0
+	tmux split-window -h
+
+	tmux select-pane -t 0
+	tmux split-window -h
+	tmux select-pane -t 2
+	tmux split-window -h
+	tmux select-pane -t 4
+	tmux split-window -h
+	tmux select-pane -t 6
+	tmux split-window -h
+
+	tmux select-pane -t 0
+
+	tmux next-layout
+	tmux next-layout
+	tmux next-layout
+	tmux next-layout
+	tmux next-layout
+fi
+
+if [ "$1" = "-R50" ]; then
+	ssh -p 9922 zhengzn@10.1.74.50
+fi
+
+if [ "$1" = "-R51" ]; then
+	ssh -p 9922 zhengzn@10.1.74.51
+fi
+
+if [ "$1" = "-R52" ]; then
+	ssh -p 9922 zhengzn@10.1.74.52
+fi
+
